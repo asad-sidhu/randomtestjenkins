4de01b33346a6d85d710b5274ef0759903b085b3
Asad Sidhu <asad_sidhu@yahoo.com>
2024-08-09 02:51:21 +0500

    Update OtherTest.java

--- src/test/java/test/OtherTest.java
+++ src/test/java/test/OtherTest.java
@@ -28,8 +28,8 @@ import org.junit.Test;
 
 public class OtherTest extends Base {
 
-    @Test public void mytest() {
-        run();
-    }
+    // @Test public void mytest() {
+    //     run();
+    // }
 
 }
