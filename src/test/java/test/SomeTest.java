f3a57dc63b9a672a1549eed1666df259d2d0e075
Asad Sidhu <asad_sidhu@yahoo.com>
2024-08-09 02:49:24 +0500

    Update SomeTest.java

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -28,17 +28,17 @@ import org.junit.Test;
 
 public class SomeTest extends Base {
 
-    @Test public void test1() {
-        run();
-    }
+    // @Test public void test1() {
+    //     run();
+    // }
 
     @Test public void test2() {
         run();
     }
 
-    @Test public void test3() {
-        run();
-    }
+    // @Test public void test3() {
+    //     run();
+    // }
 
     @Test public void test4() {
         run();
